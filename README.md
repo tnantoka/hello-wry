76cc1509567b415e8d4796e2b516e9f2ec7a2d47
tnantoka <tnantoka@bornneet.com>
2024-08-31 21:49:12 +0900

    feat: integrate JS and Rust

---
+++ README.md
@@ -0,0 +1,5 @@
+# Hello WRY
+
+Basic example of [WRY](https://github.com/tauri-apps/wry) and [TAO](https://github.com/tauri-apps/tao).
+
+![demo](/hello-wry.gif)
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,38 @@
 use tao::{
   event::{Event, StartCause, WindowEvent},
-  event_loop::{ControlFlow, EventLoop},
+  event_loop::{ControlFlow, EventLoopBuilder},
   window::WindowBuilder,
 };
-use wry::WebViewBuilder;
+use wry::{http::Request, WebViewBuilder};
 
 const INDEX_HTML: &str = include_str!("./index.html");
 
+enum UserEvent {
+  Hello(String),
+}
+
 fn main() -> wry::Result<()> {
-  let event_loop = EventLoop::new();
+  let event_loop = EventLoopBuilder::<UserEvent>::with_user_event().build();
   let window = WindowBuilder::new()
     .with_title("Hello World")
     .build(&event_loop)
     .unwrap();
-  let _webview = WebViewBuilder::new(&window)
+
+  let proxy = event_loop.create_proxy();
+  let handler = move |req: Request<String>| {
+    let body = req.body();
+    match body.as_str() {
+      _ if body.starts_with("hello:") => {
+        let color = body.replace("hello:", "");
+        let _ = proxy.send_event(UserEvent::Hello(color));
+      }
+      _ => {}
+    }
+  };
+
+  let webview = WebViewBuilder::new(&window)
     .with_html(INDEX_HTML)
+    .with_ipc_handler(handler)
     .build()?;
 
   event_loop.run(move |event, _, control_flow| {
@@ -26,6 +44,17 @@ fn main() -> wry::Result<()> {
         event: WindowEvent::CloseRequested,
         ..
       } => *control_flow = ControlFlow::Exit,
+      Event::UserEvent(e) => match e {
+        UserEvent::Hello(color) => {
+            let _ = webview.evaluate_script(
+                &format!(
+                    "document.body.style.backgroundColor = '{}'",
+                    color
+                )
+            );
+            println!("color: {}", color);
+        }
+      },
       _ => (),
     }
   });
